b002623212aa1c6989c91581bf5fccd44ae70f62
kjmc28 <145734887+kjmc28@users.noreply.github.com>
2023-09-28 20:30:46 -0600

    Update mi-archivo1.js
    kevin

--- mi-archivo1.js
+++ mi-archivo1.js
@@ -1,9 +1,7 @@
 console.log("hola mundo")
 console.log("Margoth Amores Varela")
-console.log("kevin mena chinchilla")
-<<<<<<< HEAD
-console.log("hola mundo 2.0")
-=======
-console.log("hola mundo 2.0")
-console.log("Kevin mena chinchilla")
->>>>>>> ea6cd5ee93af82b57119aa90ca09506769ad492c
+console.log("mena chinchilla kevin")
+console.log("hola que hace")
+
+
+
