2708f38d8bc3e5b384c3104b0f932e3a842ffca1
kjmc28 <145734887+kjmc28@users.noreply.github.com>
2023-09-28 20:45:10 -0600

    get commit

--- mi-archivo1.js
+++ mi-archivo1.js
@@ -2,6 +2,8 @@ console.log("hola mundo")
 console.log("Margoth Amores Varela")
 console.log("mena chinchilla kevin")
 console.log("hola que hace")
-
+console.log("lolo")
+console.log("special edition")
+console.log("holaaa")
 
 
